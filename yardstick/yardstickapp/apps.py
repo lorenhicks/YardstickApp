261565af650754b557db1e1d79dd54cbd44b9c31
Loren Hicks <lorenhicks@gmail.com>
2023-12-30 01:12:23 -0500

    Init Django REST

---
+++ yardstick/yardstickapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YardstickappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'yardstickapp'
---
+++ yardstick/yardstickapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def main(request):
+    return HttpResponse('M-i-c K-e-y M-o-u-s-e.')
