4aed12d7b5618aecf905f2e46c43243078e7df9a
Loren Hicks <lorenhicks@gmail.com>
2023-12-30 23:04:32 -0500

    Init CreateAPIView

--- yardstick/yardstickapp/views.py
+++ yardstick/yardstickapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
-def main(request):
-    return HttpResponse('M-i-c K-e-y M-o-u-s-e.')
+
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    
